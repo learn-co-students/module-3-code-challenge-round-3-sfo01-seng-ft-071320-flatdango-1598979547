f294cf7d98db5d24c0deddb83d58e85028b3f6ea
Shannon Nabors <shannon.nabors@gmail.com>
2020-07-24 12:43:33 -0400

    removed query selectors from index.js to avoid confusion

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
 const url = "http://localhost:3000/films"
-const filmsDiv = () => document.querySelector("#films")
-const posterDiv = () => document.querySelector("#poster")
-const showingDiv = () => document.querySelector("#showing")
